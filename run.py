ea8aae95f39fb01dc275c2485dad19f8af2ccc3c
William Clavier <wcinvents28@gmail.com>
2020-04-13 16:10:48 -0400

    Updated For Testing

--- run.py
+++ run.py
@@ -1,7 +1,23 @@
 # this is currently for testing but will be where the main app is soon!!!
 from util.broker import LiveBroker
+from datetime import datetime as dt
+from time import sleep
 
 Broker = LiveBroker()
 # Broker.Buy("GOOG", 6)
 # Broker.Sell("GOOG", 5)
-print(Broker.Holdings())
+try:
+    Start = Broker.startBalance
+    while True:
+        Holdings = Broker.Holdings()
+        Percent = round(((Holdings - Start) / Start) * 100, 4)
+        if Percent > 0:
+            percentString = "+{}%".format(Percent)
+        else:
+            percentString = "{}%".format(Percent)
+        print("{} --> $ {}    ({})".format(
+            dt.now().strftime("%Y-%m-%d %H:%M:%S"),
+            Holdings, percentString))
+        sleep(1)
+except KeyboardInterrupt:
+    pass
